620b506d32929b036ed02c5872f19965d580bef5
Jill Moraca <jmoraca@princeton.edu>
2018-03-26 18:16:24 -0400

    Adding a js menu

---
+++ scripts/jills-script.js
@@ -0,0 +1,6 @@
+document.getElementById("navMenu").innerHTML =
+ '<ul>'+
+  '<li><a href="index.html">Home</a></li>'+
+  '<li><a href="services.html">Subpage01</a></li>'+
+  '<li><a href="about.html">Subpage02</a></li>'+
+ '</ul>';
